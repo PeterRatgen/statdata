fd134f8daa663193b0fd5f16f46a816b8464146a
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-11-21 00:32:48 +0100

    r document

---
+++ aktivitet_2/aktivitet_2.R
@@ -0,0 +1,17 @@
+record <- read.csv("~/documents/statdata/aktivitet_2/data.csv", sep = ";", header = TRUE)
+
+options(scipen = 10)
+
+# Histogram for TREATCOST
+h <- hist(record$TREATCOST)
+pdf("treatcost.pdf", width = 10, height = 8);
+plot(h, main = "Omkosting for behandling per patient i kr.",
+     xlab = "Omkostning per patient i kr.",
+      ylab = "Frekvens", col = "blue", xaxt = "n")
+axis(side = 1, at = axTicks(1),
+    labels = formatC(axTicks(1), format = "d", big.mark = ","))
+dev.off()
+
+# Opsummering af data
+summary(record[2])
+sd(unlist(record[2]))

6db45ce41a0be2ee5d9a01b31813d1df294d4723
Camilla E. Petersen <61732390+CamillaEPetersen@users.noreply.github.com>
2020-11-21 17:38:56 +0100

    additions to .R

--- aktivitet_2/aktivitet_2.R
+++ aktivitet_2/aktivitet_2.R
@@ -1,10 +1,10 @@
-record <- read.csv("~/documents/statdata/aktivitet_2/data.csv", sep = ";", header = TRUE)
+record <- read.csv("./data.csv", sep = ";", header = TRUE)
 
 options(scipen = 10)
 
 # Histogram for TREATCOST
 h <- hist(record$TREATCOST)
-pdf("treatcost.pdf", width = 10, height = 8);
+pdf("plots/treatcost.pdf", width = 10, height = 8);
 plot(h, main = "Omkosting for behandling per patient i kr.",
      xlab = "Omkostning per patient i kr.",
       ylab = "Frekvens", col = "blue", xaxt = "n")
@@ -16,4 +16,57 @@ dev.off()
 summary(record[2])
 sd(unlist(record[2]))
 
-r <- rnorm(500, mean = 78754, sd = 55608.07)
+treatMean = mean(unlist(record[2]))
+treatSd = sd(unlist(record[2]))
+
+error = qnorm(0.95)*treatSd/sqrt(length(unlist(record[2])))
+
+#venstre
+treatMean - error
+#højre
+treatMean+error
+
+# sandsynligheden for at en behandling koster over 95000 kr
+# dette er den teoretiske sandsynlighed 
+under95k  <- 1- pnorm(q = 95000, mean = treatMean, sd = treatSd)
+
+# sandsynligheden for at en behandling koster over 95K
+sum(record$TREATCOST > 95000)/length(record$TREATCOST)
+
+
+under40K <- sum(record$TREATCOST < 40000)/length(record$TREATCOST)
+over65K <- sum(record$TREATCOST < 65000)/length(record$TREATCOST)
+
+# den endelige sandsynlighed
+1 - under40K - over65K
+
+
+#overblik over data
+pdf("plots/caredaysScatterPlot.pdf", width = 10, height = 8)
+plot(record$CAREDAYS, record$TREATCOST, pch = 19, xlab = "Dage brugt på hospitalet", ylab = "Behandlingsomkostninger i kr.", main = "Behandlingsomkostninger til antal dage brugt på hospitalet")
+dev.off()
+
+pdf("plots/caredaysScatterPlotAbline.pdf", width = 10, height = 8)
+plot(record$CAREDAYS, record$TREATCOST, pch = 19, xlab = "Dage brugt på hospitalet", ylab = "Behandlingsomkostninger i kr.", main = "Behandlingsomkostninger til antal dage brugt på hospitalet")
+abline(lm(record$TREATCOST~record$CAREDAYS), col = "blue")
+dev.off()
+
+
+linearModel <-lm(record$TREATCOST~record$CAREDAYS)
+
+
+summary(linearModel)
+
+pdf("plots/caredaysResiduals.pdf", width = 10, height = 8)
+plot(linearModel$residuals)
+dev.off()
+
+
+lm(TREATCOST~MEDICINE+LAB+XRAY+INHALATOR+STATUS+CAREDAYS+INTENSIVEDAYS+AGE+GENDER+INSURANCE, data = record)
+
+corretlationMatrix <- cor(record[,c(2:12)])
+round(corretlationMatrix, 2)
+
+
+
+

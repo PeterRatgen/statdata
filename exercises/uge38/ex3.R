78ae79a4b888c8c5c7837af687e44e088f6971d2
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-12-29 12:14:36 +0100

    ass

--- exercises/uge38/ex3.R
+++ exercises/uge38/ex3.R
@@ -19,4 +19,13 @@ calc_steak <- function(dataset){
 }
 
 kobe_streak <- calc_steak(kobe$basket)
+table(kobe_streak)
 barplot(table(kobe_streak))
+
+
+outcomes <- c("H", "M")
+sim_basket <- sample(outcomes, size = 133, replace = TRUE)
+table(calc_steak(sim_basket))
+pdf("sim_barplot.pdf", width = 10, height = 10)
+barplot(table(calc_steak(sim_basket)))
+dev.off()
